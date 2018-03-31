c9718982633d490276388c8b8b0c8b7e9d416196
pranavj1001 <pranavj1001@gmail.com>
2018-03-31 22:23:34 +0530

    setState for the searchInput

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { searchQuery: '' };
+  }
+
+  onInputChange(event) {
+    this.setState({ searchQuery: event.target.value });
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Search for a city"
+          className="form-control"
+          value={this.state.searchQuery}
+          onChange={this.onInputChange.bind(this)}
+        />
         <span className="input-group-btn">
           <button className="btn btn-primary" type="submit">Search</button>
         </span>
