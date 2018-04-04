9c2783faf0a55256fef45f62226701aabfe0c28b
pranavj1001 <pranavj1001@gmail.com>
2018-04-04 23:22:00 +0530

    show average line on the graph

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 export default (props) => (
     <div>
       <Sparklines height={100} width={150} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
     </div>
   );
