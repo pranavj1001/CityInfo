96ee40c55c8622d21bb2b5c2ec4c0a403e98fed4
pranavj1001 <pranavj1001@gmail.com>
2018-04-05 22:53:41 +0530

    add cityMap component

---
+++ src/components/city_map.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class CityMap extends Component {
+  componetDidMount() {
+    new google.maps.Map(this.refs.map, {
+      zoom: 12,
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lon
+      }
+    });
+  }
+
+  render() {
+    return <div ref="map" />;
+  }
+}
+
+export default CityMap;
